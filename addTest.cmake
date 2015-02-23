e3f25f8b1e516c958f3b1a4656c0b85b96a41e79
Eldar Zakirov <unixod@gmail.com>
2015-02-23 12:29:10 +0300

    refactoring precompileCatch: Catch unit-tests frameword precompilation features added

--- addTest.cmake
+++ addTest.cmake
@@ -1,5 +1,7 @@
+include(CMakeParseArguments)
+
 #
-# addTest(test_name file_stem)
+# addTest(test_name file_stem [OBJECT_LIBS libs...] [[LIBS] libs...])
 #
 # Parameters:
 # test_name     -   test description string
@@ -12,15 +14,21 @@
 # and add this binary as test.
 #
 function(addTest test_name file_stem)
+    cmake_parse_arguments(DEPS "" "" "LIBS;OBJECT_LIBS" ${ARGN})
+
     set(CPP_SOURCE_FILE_NAME
         "test_${file_stem}.cpp")
 
     set(TEST_FILE_NAME
         "${file_stem}.test")
 
-    add_executable(${TEST_FILE_NAME} ${CPP_SOURCE_FILE_NAME})
+    add_executable(${TEST_FILE_NAME}
+        ${CPP_SOURCE_FILE_NAME}
+        ${DEPS_OBJECT_LIBS})
 
-    target_link_libraries(${TEST_FILE_NAME} ${ARGN})
+    target_link_libraries(${TEST_FILE_NAME}
+        ${DEPS_LIBS}
+        ${DEPS_UNPARSED_ARGUMENTS})
 
     add_test(${test_name} ${TEST_FILE_NAME})
 endfunction()
