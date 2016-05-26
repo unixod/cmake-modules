a176dd96a79421774efe9b929cedeb93da73ca23
Eldar Zakirov <unixod@gmail.com>
2016-05-26 17:30:39 +0400

    showFilesInIDE, file-patterns support added

--- showFilesInIDE.cmake
+++ showFilesInIDE.cmake
@@ -1,8 +1,8 @@
 #
-# showFilesInIDE(file_paths...)
+# showFilesInIDE(file_patterns...)
 #
 # Parameters:
-# file_paths... -   files that shall be accessible from IDE
+# file_patterns... -   files to show in IDE
 #
 # Description:
 # Include directory content for convenience in editing in IDE
@@ -11,6 +11,7 @@ macro(showFilesInIDE)
     string(MD5 TMP_TARGET_NAME ${CMAKE_CURRENT_LIST_FILE})
 
     math(EXPR TMP_TARGETS_COUNTER "0${TMP_TARGETS_COUNTER} + 1")
-    add_custom_target("${TMP_TARGET_NAME}${TMP_TARGETS_COUNTER}" ALL SOURCES ${ARGN})
+    file(GLOB_RECURSE FILES_TO_SHOW ${ARGN})
+    add_custom_target("${TMP_TARGET_NAME}${TMP_TARGETS_COUNTER}" ALL SOURCES ${FILES_TO_SHOW})
 endmacro()
 
