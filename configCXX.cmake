cdf0af4ff133a31d6f9175d48a336ecc7285411e
Eldar Zakirov <unixod@gmail.com>
2017-01-17 11:35:57 +0400

    The support of using sanitizers added

--- configCXX.cmake
+++ configCXX.cmake
@@ -1,8 +1,5 @@
 include(selectSupportedCXXFlags)
 
-selectSupportedCXXFlags(COMPILER_WALL_FLAG
-    -Wall)
-
 selectSupportedCXXFlags(COMPILER_CXX11_FLAG
     FIRST_VALID_OF
         -std=c++11
@@ -13,3 +10,16 @@ selectSupportedCXXFlags(COMPILER_CXX14_FLAG
         -std=c++14
         -std=c++1y)
 
+if(MSVC)
+    selectSupportedCXXFlags(COMPILER_WALL_FLAG /W4)
+else()
+    selectSupportedCXXFlags(COMPILER_WALL_FLAG -Wall)
+
+    selectSupportedCXXFlagsToString(COMPILER_ENABLE_SANITIZERS_FLAG
+        -fno-sanitize-recover=all
+        -fsanitize=address
+        -fsanitize=undefined
+        -g
+        -fno-omit-frame-pointer
+        -O1)
+endif()
